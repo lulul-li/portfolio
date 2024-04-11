e0d816154710d49c429fdc70967e9f0c3126abeb
lulu <lulu.li@titansoft.com.sg>
2024-04-11 19:15:04 +1000

    integrate ig on about page

--- src/components/about.js
+++ src/components/about.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const About = () => (
-    <div className="home">
-        <h1>Welcome to about</h1>
-        // Hero image and introduction text here
-    </div>
-);
+const About = () => {
+    const [instagramPosts, setInstagramPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchInstagramPosts = async () => {
+            try {
+                const response = await axios.get('https://graph.instagram.com/me/media?fields=id,media_url,caption&access_token=0b63505e1ecf9f194c5fb158b9e9cf33');
+                setInstagramPosts(response.data.data);
+            } catch (error) {
+                console.error('Error fetching Instagram posts:', error);
+            }
+        };
+
+        fetchInstagramPosts();
+    }, []);
+
+    return (
+        <div>
+            <h2>About Me</h2>
+            <p>Here's some information about me...</p>
+
+            <h3>My Latest Instagram Posts</h3>
+            <div className="instagram-posts">
+                {instagramPosts.map(post => (
+                    <div key={post.id} className="instagram-post">
+                        <img src={post.media_url} alt={post.caption} />
+                        <p>{post.caption}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
 
 export default About;
